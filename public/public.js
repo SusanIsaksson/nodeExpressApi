44512b07f524842bc8921555135b7f7168c2f5d1
susanisaksson <susan.isaksson@medieinstitutet.se>
2021-09-03 10:51:36 +0200

    Add a class för the body and an id for the output of the extern API.. Add function in js to delete the color-array before new output, and not just have the array repeat. In the extern API have a delay of the output between the first part of the joke and the punchline. And some styling of the frontend.

--- public/public.js
+++ public/public.js
@@ -1,27 +1,23 @@
-function helloName() {
-    //get the value of the input id="nameInput"
-    let x = document.getElementById("nameInput").value;
-    
-    //if nameInput is not set
-    if (x == "") {
-        alert("Please enter your name!");
-    } else {
-        document.getElementById("hello").innerHTML = "Välkommen " + x;
-        //alert ("Välkommen " + x);
-    }
-}
-
 async function getSomething() {
     console.log("GET")
+    if (document.getElementById("viewColor").innerHTML) {
+        delOldColor()
+    }   
     const colorToView = await makeRequest("http://localhost:3000/api", "GET")
         for (let i = 0; i < colorToView.length; i++) {
+            
             const element = document.createElement('h4')
             element.innerText = colorToView[i].title
-
+           
             document.getElementById("viewColor").appendChild(element)
             console.log(element)
-
+            
         }
+}
+
+async function delOldColor() {
+    console.log("Ta bort gammal")
+    document.getElementById("viewColor").innerHTML = "";
 }
 
 async function postNewColor() {
@@ -31,15 +27,46 @@ async function postNewColor() {
     const status = await makeRequest("http://localhost:3000/api", "POST", {title: newColor})
     console.log(status) 
 }
-
+let a = false
 async function getFromExternAPI() {
+    if (a) {
+        return
+    }
+    a = true
+    if (document.getElementById("jokeDiv")) {
+        document.getElementById("jokeDiv").innerHTML = "";
+    }
     console.log("GET API")
     let response = await fetch("https://official-joke-api.appspot.com/random_joke")
-    let result = await response.json()
-    console.log(result) 
-    
+    let body = await response.json()
+    //setup + punchline
+    console.log(body.setup)
+    setTimeout(() => {
+    console.log(body.punchline)
+    }, 1000);
     
 
+    let x = document.createElement("h4")
+    x.innerText = body.setup
+
+    let y = document.createElement("h2")
+    y.innerText = body.punchline
+    //setTimeout(body.punchline, 3000);
+
+    document.getElementById("jokeDiv").appendChild(x)
+    setTimeout(() => {
+        document.getElementById("jokeDiv").appendChild(y)
+        a = false
+    }, 1000); 
+   
+    
+
+    
+
+}
+
+function clearValue() {
+    document.getElementById("").value = ""
 }
 
 
