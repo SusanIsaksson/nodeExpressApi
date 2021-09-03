ebc06d65f0ae4ae8b7013b713cdebfe2c2386383
susanisaksson <susan.isaksson@medieinstitutet.se>
2021-09-03 10:53:36 +0200

    Add a res.json(true) in the try-part and a res.status(500).json(false) the the catch part. Some cleaning in the storage.json-file. Update the tests.rest-file.

--- index.js
+++ index.js
@@ -17,10 +17,11 @@ server.post('/api', (req, res) => {
       let colorInput = JSON.parse(raw)
       colorInput.push(req.body)
       fs.writeFileSync("storage.json", JSON.stringify(colorInput))
-      
+      res.json(true)
       
   } catch(err) {
     console.error(err)
+    res.status(500).json(false)
   }
     console.log(req.body)
   
